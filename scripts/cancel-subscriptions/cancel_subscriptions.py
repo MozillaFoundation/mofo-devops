bb33f6a350d688e815efbb51956944e20c5cc3d3
Christopher DeCairos <cade@mozillafoundation.org>
2020-01-08 16:08:14 -0500

    Add a paypal strategy for cancelling subscriptions. Supported by argparse

--- scripts/cancel-subscriptions/cancel_subscriptions.py
+++ scripts/cancel-subscriptions/cancel_subscriptions.py
@@ -1,68 +1,119 @@
 import sys
+import argparse
 from csv import DictReader, DictWriter
 import stripe
+import paypalrestsdk
 
-failed_subscriptions = []
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--subscription-type',
+    help='The type of subscriptions being cancelled',
+    choices=['stripe', 'paypal']
+    required=True,
+    dest='type'
+)
+parser.add_argument(
+    '--csv-filename',
+    help='The file where subscriptions are listed',
+    required=True,
+    dest='filename'
+)
+parser.add_argument(
+    '--stripe-key',
+    help='Stripe API key. must be specified if type is "stripe"',
+    dest='stripe_key'
+
+)
+
+args = parser.parse_args()
+
+failed_transactions = []
 
 # Cancels all subscriptions specified in the CSV "filename"
 def cancel_all_subscriptions(filename):
-    print(f'Loading subscription ids from {filename}')
-    subscription_ids = load_subscription_ids(filename)
+    print(f'Loading ids from {filename}')
+    ids = load_ids(filename)
 
-    print(f'Cancelling {len(subscription_ids)} subscriptions...')
-    for subscription_id in subscription_ids:
-        cancel_subscription_by_id(subscription_id)
+    # use the selected cancellation strategy
+    cancellation_strategy = cancel_stripe_subscription if args.type is 'stripe' else cancel_paypal_billing_agreement
 
-    if len(failed_subscriptions) > 0:
-        print(f'{len(failed_subscriptions)} failed to cancel, outputting to {filename}-errors.csv')
-        write_errors(failed_subscriptions, f'{filename}-errors.csv')
+    print(f'Cancelling {len(ids)} subscriptions...')
+    for id in ids:
+        cancellation_strategy(id)
+
+    if len(failed_transactions) > 0:
+        print(f'{len(failed_transactions)} failed to cancel, outputting to {filename}-errors.csv')
+        write_errors(failed_transactions, f'{filename}-errors.csv')
         exit(1)
 
     print('Successfully cancelled all subscriptions!')
     exit(0)
 
-# Cancel the given subscription using its ID
-def cancel_subscription_by_id(subscription_id):
+# Cancel the given Stripe subscription using its ID
+def cancel_stripe_subscription(subscription_id):
     try:
         canceled = stripe.Subscription.delete(subscription_id)
-    except stripe.error.StripeError as e:
-        failed_subscriptions.append(subscription_id)
-    except Exception as e:
-        failed_subscriptions.append(subscription_id)
 
-    if not canceled or canceled.status != 'canceled':
-        failed_subscriptions.append(subscription_id)
+        if not canceled or canceled.status is not 'canceled':
+            failed_transactions.append(subscription_id)
 
-# Open the CSV file and read in all the subscription IDs
-def load_subscription_ids(filename):
+    except (stripe.error.StripeError, Exception) as e:
+        failed_transactions.append(subscription_id)
+        print(f'Failed to cancel subscription f{subscription_id}. Error: ${e}')
+
+# Cancel the given Paypal Billing Agreement using its ID
+def cancel_paypal_billing_agreement(billing_agreement_id):
+    try:
+        billing_agreement = paypalrestsdk.Payments.BillingAgreement.find(billing_agreement_id)
+        cancel_note = {'note': 'MZLA migration'}
+
+        if not billing_agreement.cancel(cancel_note):
+            failed_transactions.append(billing_agreement_id)
+            print(f'Failed to cancel Billing Agreement ${billing_agreement_id}. Error: ${billing_agreement.error}')
+
+    except ResourceNotFound as error:
+        failed_transactions.append(billing_agreement_id)
+        print(f'Failed to find Billing Agreement ${billing_agreement_id}.')
+
+# Open the CSV file and read in all the ids
+def load_ids(filename):
     with open(filename, 'rb') as csv_file:
         reader = DictReader(csv_file)
-        subscription_ids = []
+        ids = []
+        row_name = ''
+
+        if args.type is 'stripe':
+            row_name = 'subscription_id'
+        else:
+            row_name = 'Reference Txn ID'
 
         for row in reader:
-            subscription_ids.append(row['subscription_id']
+            ids.append(row[row_name]
 
-        return subscription_ids
+        return ids
 
 # output any failed subscription cancellations to errors.csv
-def write_errors(subscription_ids, filename):
-    field_name = 'subscription_id'
+def write_errors(ids, filename):
+    field_name = 'ids'
 
     with open(filename, 'w') as csv_file:
         writer = DictWriter(csv_file, fieldname=[field_name])
         writer.writeheader()
 
-        for subscription_id in subscription_ids:
-            writer.writerow({field_name: subscription_id})
+        for id in ids:
+            writer.writerow({field_name: id})
 
 if __name__ == '__main__':
-    if not sys.argv[1] or not sys.argv[2]:
-        print('You must provide a path to a CSV file containing subscription IDs')
-        exit(1)
 
-    # Configure Stripe, the first script argument is the Stripe API key
-    stripe.api_key = sys.argv[1]
-    stripe.max_network_retries = 3
+    if args.type is 'stripe'
+        if not args.stripe_key:
+            print('please provide a Stripe API key with --stripe-key')
+            exit(1)
+
+        # Configure Stripe, the first script argument is the Stripe API key
+        stripe.api_key = args.stripe_key
+        stripe.max_network_retries = 3
 
     # Start cancelling subscriptions using the CSV file specified as the second script parameter
-    cancel_all_subscriptions(sys.argv[2])
+    cancel_all_subscriptions(args.filename)
